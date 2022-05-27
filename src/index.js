ba94caf46e95b2f4b63f041f0427ba052722934d
Victoria Tejeda <vicyatm@gmail.com>
2022-05-27 13:43:05 -0500

    instalcion de riot hot render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import * as riot from 'riot'
 import './style.css'
-import '@riotjs/hot-reload'
-import { mount } from 'riot'
+import  reload from '@riotjs/hot-reload'
 import registerGlobalComponents from './register-global-components'
 import App from './app.riot'
+import { setBase } from '@riotjs/route'
+import { initDomListeners } from '@riotjs/route'
+
+ setBase('/')
+// refresh all the DOM nodes called 'my-component' without losing their state
+reload(App)
+
+initDomListeners(document.querySelector('.main-root'))
 
 
 const myApp= riot.component(App)
